a79efe9e5339d75f79d6d40b4c11868c25661b15
steve <StevencNewbern@gmail.com>
2025-03-05 06:40:19 -0600

    Initial commit

---
+++ SaaS_Allowance_Solver/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for SaaS_Allowance_Solver project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SaaS_Allowance_Solver.settings')
+
+application = get_wsgi_application()
